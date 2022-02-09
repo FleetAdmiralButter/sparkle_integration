cf7d221deafafa573b58e060238eb651496cad45
FleetAdmiralButter <ming.quah@protonmail.com>
2022-02-09 23:17:59 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # sparkle_integration
+
+Drupal-Sparkle-QuantCDN integration for the XIV on Mac app.
+
+Provides the supporting services necessary to push new update data to the XIV on Mac app.
+
+sparkle_integration relies on the following:
+
+* Drupal 9
+* A fully configured QuantCDN module
+
+*NOTE:* This module is currently in a pre-alpha state and purpose-specific to the XIV on Mac project, but I plan to make everything properly configurable and modular once I have a better understanding of how Sparkle works. Don't expect it to work if you install this on any other site.
---
+++ src/Form/SparkleForm.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\sparkle_integration\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\file\Entity\File;
+use Drupal\Core\File\FileSystem;
+use Drupal\quant\Plugin\QueueItem\RouteItem;
+
+class SparkleForm extends FormBase {
+
+    public function buildForm(array $form, FormStateInterface $form_state) {
+        $form['appcast'] = [
+            '#type' => 'managed_file',
+            '#title' => $this->t('AppCast XML file'),
+            '#description' => $this->t('Upload a new AppCast XML. The existing file will be replaced automatically.'),
+            '#upload_location' => 'private://sparkle_staging',
+            '#upload_validators' => [
+                'file_validate_extensions' => ['xml'],
+            ],
+        ];
+
+        $form['update'] = [
+            '#type' => 'managed_file',
+            '#title' => $this->t('Update package'),
+            '#description' => $this->t('Update package. This will be placed alongside the AppCast XML feed.'),
+            '#upload_location' => 'private://sparkle_staging',
+        ];
+
+        $form['replace_package'] = [
+            '#type' => 'checkbox',
+            '#title' => $this->t('Replace Update Package'),
+            '#default_value' => TRUE,
+            '#description' => $this->t('If an update package with the same name already exists, it will be replaced with the uploaded copy.'),
+        ];
+
+        $form['actions']['#type'] = 'actions';
+        $form['actions']['submit'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('Confirm Release'),
+          '#button_type' => 'primary',
+        ];
+
+        return $form;
+    }
+
+    public function getFormId() {
+        return 'sparkle_integration.release';
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+        // Make sure the file system is in a sane state.
+        $fs_driver = \Drupal::service('file_system');
+        $directory = 'public://update_data';
+        $fs_driver->prepareDirectory($directory, \Drupal\Core\File\FileSystemInterface::CREATE_DIRECTORY);
+
+        $appcast_file_name = 'xivonmac_appcast.xml';
+        $appcast_file_location = 'public://update_data/' . $appcast_file_name;
+        
+        // Process files
+        $appcast = $form_state->getValue('appcast', 0);
+        if (isset($appcast[0]) && !empty($appcast[0])) {
+            $file = File::load($appcast[0]);
+            $new_appcast_file_location = 'private://sparkle_staging/' . $file->getFilename();
+            \Drupal::service('messenger')->addMessage('Got file name: ' . $file->getFilename());
+
+            // Copy the new AppCast feed into place.
+            copy($new_appcast_file_location, $appcast_file_location);
+        }
+
+        // Finish by hydrating the CDN with new content.
+        try {
+            $route = '/sites/default/files/update_data/xivonmac_appcast.xml';
+            $item = new RouteItem(['route' => $route]);
+            $item->send();
+            \Drupal::service('messenger')->addMessage("CDN hydration started");
+        } catch (\Exception $e) {
+            \Drupal::service('messenger')->addError("Error connecting to QuantCDN");
+        }
+
+        
+    }
+}
