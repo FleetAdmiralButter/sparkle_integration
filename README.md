9cd2d349b4828140ffdabd1f503023e7da5f5cc0
FleetAdmiralButter <ming.quah@protonmail.com>
2022-03-07 00:37:10 +1100

    Beta: XIVLauncher Patching endpoint

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
 # sparkle_integration
 
-Drupal-Sparkle-QuantCDN integration for the XIV on Mac app.
+## Introduction
 
-Provides the supporting services necessary to push new update data to the XIV on Mac app.
+Provides auto-update services for XIV on Mac.
 
-sparkle_integration relies on the following:
+This module exposes a form that allows for new data to be pushed to various update API endpoints.
+
+Currently, auto-updates for the following components are supported:
+
+1) The XIV on Mac app itself, via the Sparkle framework.
+2) XIVLauncher Patcher.
+
+Endpoints are as follows:
+
+Sparkle version feed: https://www.xivmac.com/sites/default/files/update_data/xivonmac_appcast.xml
+XIVLauncher Patcher version info: https://www.xivmac.com/sites/default/files/seventh_dawn/version.txt
+
+## Requirements
+This module currently relies on the following:
 
 * Drupal 9
-* A fully configured QuantCDN module
+* Fully configured QuantCDN integration
+* PHP 8.x or higher
 
-*NOTE:* This module is currently in a pre-alpha state and purpose-specific to the XIV on Mac project, but I plan to make everything properly configurable and modular once I have a better understanding of how Sparkle works. Don't expect it to work if you install this on any other site.
+*NOTE:* This module is purpose-specific to the XIV on Mac project and is not intended for installation on other sites.
+
+## Security
+
+The update system relies on Drupal's authentication and role-based access control middleware to mediate access.
+
+In addition to the above, Drupal itself handles brute-force and CSRF protection.
+
+App binaries are code signed with an Apple Developer certificate and pushed to the CDN platform via HTTPS.
+
+**Only** project leads have the requisite access to push update data.
+
+Please visit https://www.xivmac.com/.well-known/security.txt for up to date contact information to disclose security vulnerabilities.
--- src/Form/SparkleForm.php
+++ src/Form/SparkleForm.php
@@ -14,22 +14,58 @@ use Drupal\Core\Archiver\ArchiverException;
 
 class SparkleForm extends FormBase {
 
+    /**
+     * Build the form layout and prepare the staging folder.
+     */
     public function buildForm(array $form, FormStateInterface $form_state) {
-        $form['appcast'] = [
+        $staging_directory = 'private://release_manager_staging';
+        \Drupal::service('file_system')->prepareDirectory($staging_directory, \Drupal\Core\File\FileSystemInterface::CREATE_DIRECTORY | \Drupal\Core\File\FileSystemInterface::MODIFY_PERMISSIONS);
+
+        $form['sparkle'] = [
+            '#title' => $this->t('App Updates'),
+            '#type' => 'fieldset',
+        ];
+        
+        $form['sparkle']['appcast'] = [
             '#type' => 'managed_file',
             '#title' => $this->t('AppCast XML file'),
-            '#description' => $this->t('Upload a new AppCast XML. The existing file will be replaced automatically.'),
-            '#upload_location' => 'private://sparkle_staging',
+            '#description' => $this->t('Upload a new AppCast XML. The file will be renamed and existing file will be replaced automatically.'),
+            '#upload_location' => 'private://release_manager_staging',
             '#upload_validators' => [
                 'file_validate_extensions' => ['xml'],
             ],
         ];
 
-        $form['update'] = [
+        $form['sparkle']['update'] = [
             '#type' => 'managed_file',
             '#title' => $this->t('Update package'),
             '#description' => $this->t('A zip file containing the main update package (.tar.xz) as well as delta (.delta) files.'),
-            '#upload_location' => 'private://sparkle_staging',
+            '#upload_location' => 'private://release_manager_staging',
+            '#upload_validators' => [
+                'file_validate_extensions' => ['zip'],
+            ],
+        ];
+
+        $form['patcher'] = [
+            '#title' => $this->t('Patcher Updates'),
+            '#type' => 'fieldset',
+        ];
+
+        $form['patcher']['patcher_version_feed'] = [
+            '#type' => 'managed_file',
+            '#title' => $this->t('Patcher version feed'),
+            '#description' => $this->t('A txt file container the patcher version info. The file will be renamed and existing file will be replaced automatically.'),
+            '#upload_location' => 'private://release_manager_staging',
+            '#upload_validators' => [
+                'file_validate_extensions' => ['txt'],
+            ],
+        ];
+
+        $form['patcher']['patcher_update'] = [
+            '#type' => 'managed_file',
+            '#title' => $this->t('Patcher update package'),
+            '#description' => $this->t('A zip file container the patcher update data. The file will be renamed and existing file will be replaced automatically.'),
+            '#upload_location' => 'private://release_manager_staging',
             '#upload_validators' => [
                 'file_validate_extensions' => ['zip'],
             ],
@@ -49,6 +85,17 @@ class SparkleForm extends FormBase {
         return 'sparkle_integration.release';
     }
 
+    /**
+     * Process the submitted files.
+     * This function does a few things:
+     * 1) Ingest the uploaded XML, ZIP, TXT files into a private staging folder.
+     * 2) If the file is a ZIP, extract only relevant content (ignoring _MACOSX directories).
+     * 3) Copy all files into the relevant public folder (either update_data or seventh_dawn).
+     * 4) Notify the CDN of new content and push any updates files.
+     * 5) Clean up the staging folder.
+     * 
+     * There is some duplicated code in here which should be cleaned up, but for now everything is stable.
+     */
     public function submitForm(array &$form, FormStateInterface $form_state) {
         
         $batch = [
@@ -61,10 +108,12 @@ class SparkleForm extends FormBase {
         
         $appcast = $form_state->getValue('appcast', 0);
         $package = $form_state->getValue('update', 0);
-
+        $patcher_feed = $form_state->getValue('patcher_version_feed', 0);
+        $patcher_update = $form_state->getValue('patcher_update', 0);        
+        
         if (isset($appcast[0]) && !empty($appcast[0])) {
             $appcast_file = File::load($appcast[0]);
-            $source = 'private://sparkle_staging/' . $appcast_file->getFilename();
+            $source = 'private://release_manager_staging/' . $appcast_file->getFilename();
             $destination = 'public://update_data/xivonmac_appcast.xml';
             $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'copyFile'], [$source, $destination]];
             $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'hydrateCDN'], ['/sites/default/files/update_data/xivonmac_appcast.xml']];
@@ -83,10 +132,10 @@ class SparkleForm extends FormBase {
                     array_push($files_to_copy, $archive_file);
                 }
             }
-            $zip->extract('private://sparkle_staging/', $files_to_copy);
+            $zip->extract('private://release_manager_staging/', $files_to_copy);
 
             foreach ($files_to_copy as $file_to_copy) {
-                $source = 'private://sparkle_staging/' . $file_to_copy;
+                $source = 'private://release_manager_staging/' . $file_to_copy;
                 $destination = 'public://update_data/' . $file_to_copy;
                 $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'copyFile'], [$source, $destination]];
                 $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'hydrateCDN'], ['/sites/default/files/update_data/' . $file_to_copy]];
@@ -94,6 +143,27 @@ class SparkleForm extends FormBase {
             $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'clean'], [$package_file]];
 
         }
+
+
+        if (isset($patcher_feed[0]) && !empty($patcher_feed[0])) {
+            $patcher_feed_file = File::load($patcher_feed[0]);
+            $source = 'private://release_manager_staging/' . $patcher_feed_file->getFilename();
+            $destination = 'public://seventh_dawn/version.txt';
+            $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'copyFile'], [$source, $destination]];
+            $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'hydrateCDN'], ['/sites/default/files/seventh_dawn/version.txt']];
+            $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'clean'], [$patcher_feed_file]];
+        }
+
+        if (isset($patcher_update[0]) && !empty($patcher_update[0])) {
+            $patcher_update_file = File::load($patcher_update[0]);
+            $source = 'private://release_manager_staging/' . $patcher_update_file->getFilename();
+            $destination = 'public://seventh_dawn/PatchInstaller.zip';
+            $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'copyFile'], [$source, $destination]];
+            $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'hydrateCDN'], ['/sites/default/files/seventh_dawn/PatchInstaller.zip']];
+            $batch['operations'][] = [['\Drupal\sparkle_integration\Form\SparkleForm', 'clean'], [$patcher_update_file]];
+        }
+
+
         if (empty($batch['operations'])) {
             \Drupal::service('messenger')->addMessage('Nothing to do!');
         } else {
@@ -102,14 +172,22 @@ class SparkleForm extends FormBase {
 
     }
 
+    /**
+     * Prepare any relevant directories and copy files.
+     */
     public static function copyFile($source, $destination) {
         $fs_driver = \Drupal::service('file_system');
         $directory = 'public://update_data';
+        $patcher_directory = 'public://seventh_dawn';
         $fs_driver->prepareDirectory($directory, \Drupal\Core\File\FileSystemInterface::CREATE_DIRECTORY | \Drupal\Core\File\FileSystemInterface::MODIFY_PERMISSIONS);
+        $fs_driver->prepareDirectory($patcher_directory, \Drupal\Core\File\FileSystemInterface::CREATE_DIRECTORY | \Drupal\Core\File\FileSystemInterface::MODIFY_PERMISSIONS);
         copy($source, $destination);
     }
 
 
+    /**
+     * Pushes new files on disk to the CDN.
+     */
     public static function hydrateCDN($url) {
         try {
             $item = new RouteItem(['route' => $url]);
@@ -120,6 +198,9 @@ class SparkleForm extends FormBase {
         }
     }
 
+    /**
+     * Delete unrequired file entities.
+     */
     public static function clean(File $file) {
         $file->delete();
     }
